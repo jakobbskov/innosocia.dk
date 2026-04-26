00bbe11c59a2ffb289f3dfe4cf19c756e7d542d3
Jakob Skov <jakobbskov@protonmail.com>
2026-04-26 22:12:47 +0200

    Add Sovereign Strength screenshots because app pages need visible previews

--- src/content.config.ts
+++ src/content.config.ts
@@ -19,6 +19,12 @@ const apps = defineCollection({
 
     repoUrl: z.string().url().optional(),
     license: z.string().optional(),
+    screenshots: z.array(z.object({
+      src: z.string(),
+      alt: z.string(),
+      title: z.string(),
+      caption: z.string(),
+    })).default([]),
   }),
 });
 
